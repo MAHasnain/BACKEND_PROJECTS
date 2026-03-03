e2f86660930f8f043965c8e25d51e52ef6a54791
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-03-03 11:22:09 +0500

    Add bcrypt and dns dependencies to package.json

--- Server-setup/db/index.js
+++ Server-setup/db/index.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config()
 
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 const dbUri = process.env.MONGODB_URI
 mongoose.connect(dbUri);
 
